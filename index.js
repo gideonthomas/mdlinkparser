92981638a92f780537833da3cb753eabf294ceb8
gideonthomas <r.gideonthomas@gmail.com>
2014-05-08 16:35:38 -0400

    Use requirejs

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-exports.parse = function(data) {
-    var links = [];
-    var list = data.match(/\[[^\]]+\]\([^)]+\)/g);
-    list.forEach(function(element, index, array) {
-        links.push((element.match(/\([^)]+\)/g))[0].replace("(", "").replace(")", ""));
-    });
-    return links;
-};
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(function(require) {
+    //The value returned from the function is
+    //used as the module export visible to Node.
+    return {
+        parse:
+        function(data) {
+            var links = [];
+            var list = data.match(/\[[^\]]+\]\([^)]+\)/g);
+            list.forEach(function(element, index, array) {
+                links.push((element.match(/\([^)]+\)/g))[0].replace("(", "").replace(")", ""));
+            });
+            return links;
+        }
+    };
+});
