6c6b89f6ef88c057366f0b4b4ef17a09b227f740
gideonthomas <r.gideonthomas@gmail.com>
2014-05-08 11:40:07 -0400

    Modified to only include parsing functionality

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require("fs").readFile(process.argv[2], "utf-8", function (err, data) {
-    if(err) throw err;
-    console.log(require("./parser").parse(data));  
-});
+exports.parse = function(data) {
+    var links = new Array();
+    var list = data.match(/\[[^\]]+\]\([^)]+\)/g);
+    list.forEach(function(element, index, array) {
+        links.push((element.match(/\([^)]+\)/g))[0].replace("(", "").replace(")", ""));
+    });
+    return links;
+}
