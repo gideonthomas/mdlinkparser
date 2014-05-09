ddc8036131c7d108065fb9b5849cb348c28591fa
gideonthomas <r.gideonthomas@gmail.com>
2014-05-09 12:51:51 -0400

    Fixed page refreshing error

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-require(["./index"],
-       function(parser) {
-           return function getLinks() {
-               var markdown = document.getElementById("markdown").value;
-               var result = parser.parse(markdown);
-               var listOfLinks = "";
-               var linkText = document.getElementById("links");
+require(["domReady", "./index"],
+       function(domReady, parser) {
+           domReady(function() {
+               document.getElementById("linkButton").onclick = function() {
+                   var markdown = document.getElementById("markdown").value;
+                   var result = parser.parse(markdown);
+                   var listOfLinks = "";
+                   var linkText = document.getElementById("links");
 
-               for(var i = 0; i < result.length; i++) {
-                   listOfLinks += result[i].toString();
-                   listOfLinks += "\n";
+                   for(var i = 0; i < result.length; i++) {
+                       listOfLinks += result[i].toString();
+                       listOfLinks += "\n";
+                   }
+
+                   console.log(listOfLinks);
+
+                   linkText.value = listOfLinks;
                }
-
-               console.log(listOfLinks);
-
-               linkText.value = listOfLinks;
-           };
+           });
        });
